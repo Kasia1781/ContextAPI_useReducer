7ee9a201764195aa3ccfebfda2ee3d7ec7243967
Kasia <kasia1781@gmail.com>
2024-05-04 20:27:32 +0200

    created Timers component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import AddTimer from './components/AddTimer';
 import Header from './components/Header';
+import Timers from './components/Timers';
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Header />
 			<main>
 				<AddTimer />
+				<Timers />
 			</main>
 		</>
 	);
