e97399880d90c0e37672007a90b4a285428da2cd
Kasia <kasia1781@gmail.com>
2024-05-05 20:41:10 +0200

    using addTimer function in AddTimer component

--- src/components/AddTimer.tsx
+++ src/components/AddTimer.tsx
@@ -2,13 +2,18 @@ import { useRef } from 'react';
 import Button from './UI/Button';
 import Form, { FormHandle } from './UI/Form';
 import Input from './UI/Input';
+import { useTimersContext } from '../store/timers-context';
 
 export default function AddTimer() {
 	const form = useRef<FormHandle>(null);
+	const { addTimer } = useTimersContext();
 
 	function handleSaveTimer(data: unknown) {
 		const extractedData = data as { name: string; duration: number };
-		console.log(extractedData);
+		addTimer({
+			name: extractedData.name,
+			duration: +extractedData.duration,
+		});
 
 		form.current?.clear();
 	}
