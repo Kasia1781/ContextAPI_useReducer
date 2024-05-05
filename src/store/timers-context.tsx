c5de5a4e311f8bad32e9f2195093d79809ea9986
Kasia <kasia1781@gmail.com>
2024-05-05 20:22:21 +0200

    added useReducer

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,8 +1,14 @@
-import { createContext, useContext, type ReactNode } from 'react';
+import { createContext, useContext, useReducer, type ReactNode } from 'react';
 
 type TimerState = {
 	isRunning: boolean;
 	timers: Timer[];
+};
+
+//dane początkowe potrzebne do useReducer
+const initialState: TimerState = {
+	isRunning: true,
+	timers: [],
 };
 
 type Timer = {
@@ -32,20 +38,65 @@ type TimersContextProviderProps = {
 	children: ReactNode;
 };
 
+type StartTimersAction = {
+	type: 'START_TIMER';
+};
+
+type StopTimersAction = {
+	type: 'STOP_TIMER';
+};
+
+type AddTimersAction = {
+	type: 'ADD_TIMER';
+};
+
+type Action = {
+	type: StartTimersAction | StopTimersAction | AddTimersAction;
+};
+function timersReducer(state: TimerState, action: Action): TimerState {
+	if (action.type === 'START_TIMER') {
+		return {
+			...state,
+			isRunning: true,
+		};
+	}
+
+	if (action.type === 'STOP_TIMER') {
+		return {
+			...state,
+			isRunning: false,
+		};
+	}
+
+	if (action.type === 'ADD_TIMER') {
+		return {
+			...state,
+			timers: [
+				...state.timers,
+				{ name: action.payload.name, duration: action.payload.duration },
+			],
+		};
+
+		return state;
+	}
+}
+
 export function TimersContextProvider({
 	children,
 }: TimersContextProviderProps) {
+	const [timersState, dispatch] = useReducer(timersReducer, initialState);
+
 	const ctx: TimersContextValue = {
-		timers: [],
-		isRunning: false,
+		timers: timersState.timers,
+		isRunning: timersState.isRunning,
 		addTimer(timerData) {
-			//...
+			dispatch({ type: 'ADD_TIMER', payload: timerData });
 		},
 		startTimer() {
-			//...
+			dispatch({ type: 'START_TIMER' });
 		},
 		stopTimer() {
-			//...
+			dispatch({ type: 'STOP_TIMER' });
 		},
 	};
 
